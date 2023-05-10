95646e7ce11d6b56d23ce9fb0280db94e9ee8108
Matheus Mattos Avelino <114790698+MatheusMA678@users.noreply.github.com>
2023-05-10 01:26:30 +0000

    rotas finalizadas

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Feather } from "@expo/vector-icons";
+
 import { Home } from "./screens/Home";
 import { VideoAulas } from "./screens/VideoAulas";
 import { Redacao } from "./screens/Redacao";
@@ -7,6 +9,7 @@ import { Perfil } from "./screens/Perfil";
 import { Baixados } from "./screens/Baixados";
 
 import { blue, gray, white } from "tailwindcss/colors";
+import { View, Text } from "react-native";
 
 const Tabs = createBottomTabNavigator();
 
@@ -16,16 +19,71 @@ export function Routes() {
       screenOptions={{
         tabBarStyle: {
           backgroundColor: blue[600],
+          height: 60,
+          paddingBottom: 10,
+          paddingTop: 10,
+          borderTopLeftRadius: 10,
+          borderTopRightRadius: 10,
         },
         tabBarActiveTintColor: white,
         tabBarInactiveTintColor: gray[300],
+
+        headerLeft: () => (
+          <View className="ml-4 rounded-full border border-blue-500 h-10 w-10"></View>
+        ),
+        headerTitle: () => (
+          <View>
+            <Text>Olá, seja bem-vindo(a)!</Text>
+            <Text className="font-bold text-xl">Matheus Mattos Avelino</Text>
+          </View>
+        ),
       }}
     >
-      <Tabs.Screen name="Home" component={Home} />
-      <Tabs.Screen name="VideoAulas" component={VideoAulas} />
-      <Tabs.Screen name="Redacao" component={Redacao} />
-      <Tabs.Screen name="Perfil" component={Perfil} />
-      <Tabs.Screen name="Baixados" component={Baixados} />
+      <Tabs.Screen
+        name="Home"
+        component={Home}
+        options={{
+          tabBarIcon: ({ size, color }) => (
+            <Feather name="home" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="Videoaula"
+        component={VideoAulas}
+        options={{
+          tabBarIcon: ({ size, color }) => (
+            <Feather name="play-circle" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="Redação"
+        component={Redacao}
+        options={{
+          tabBarIcon: ({ size, color }) => (
+            <Feather name="edit-2" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="Perfil"
+        component={Perfil}
+        options={{
+          tabBarIcon: ({ size, color }) => (
+            <Feather name="user" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="Baixados"
+        component={Baixados}
+        options={{
+          tabBarIcon: ({ size, color }) => (
+            <Feather name="download" size={size} color={color} />
+          ),
+        }}
+      />
     </Tabs.Navigator>
   );
 }
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -3,8 +3,13 @@ import React from "react";
 
 export function Home() {
   return (
-    <View>
-      <Text>Home</Text>
+    <View className="flex-1">
+      <View className="m-4 rounded-md bg-blue-500 shadow-lg h-44 p-4">
+        <Text className="font-bold text-xl text-white">Simulado Estadual</Text>
+        <Text className="text-white/50">
+          Atenção, simulado está com o horário de Brasília.
+        </Text>
+      </View>
     </View>
   );
 }
